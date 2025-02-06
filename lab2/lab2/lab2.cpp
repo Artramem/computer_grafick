4d0a4df8ebcd897ea9c277adcd02f45d2d47fb3d
Artramem <69641597+Artramem@users.noreply.github.com>
2025-02-06 20:48:12 +0300

    Add files via upload

--- lab2/lab2/lab2.cpp
+++ lab2/lab2/lab2.cpp
@@ -6,17 +6,20 @@ using namespace std;
 
 
 Mat floydSteinbergDithering(Mat& img, int n) {
+    int levels = 1 << n;
 
-    Mat out_img = Mat::zeros(img.size(), CV_8U);  
+    Mat out_img = img.clone();  
     for (int y = 0; y < img.rows; y++) {
         for (int x = 0; x < img.cols; x++) {
 
             
             uchar pixel = img.at<uchar>(y, x);
 
-            
-            uchar quantized_pixel = pixel >> (8 - n) << (8 - n);
+            int step = 255 / (levels - 1);
+            int step1 = 255 / levels + 1;
+            uchar quantized_pixel = pixel / step1 * step;
             int error = pixel - quantized_pixel;
+            out_img.at<uchar>(y, x) = quantized_pixel;
 
             if (x + 1 < img.cols) {
                 
@@ -34,9 +37,6 @@ Mat floydSteinbergDithering(Mat& img, int n) {
                     img.at<uchar>(y + 1, x + 1) = std::min(255, std::max(0, img.at<uchar>(y + 1, x + 1) + error * 1 / 16));
                 }
             }
-
-            
-            out_img.at<uchar>(y, x) = quantized_pixel;
         }
     }
 
@@ -45,7 +45,8 @@ Mat floydSteinbergDithering(Mat& img, int n) {
 
 
 int main() {
-    Mat src = imread("kitten.png", IMREAD_GRAYSCALE); 
+    Mat src = imread("kitten.png", IMREAD_GRAYSCALE);
+    Mat ini = imread("initial.png", IMREAD_GRAYSCALE);
 
     if (src.empty()) {
         cerr << "Не удалось загрузить изображение!" << endl;
@@ -76,6 +77,10 @@ int main() {
     n = 4;
     Mat dst4 = floydSteinbergDithering(src, n);
     imwrite("kitten_output4.png", dst4);
+
+    n = 1;
+    Mat dst5 = floydSteinbergDithering(ini, n);
+    imwrite("output.png", dst5);
 
     waitKey(0);
     destroyAllWindows();
